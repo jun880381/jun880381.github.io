d76bde066cb83d8c07449ecfe3bc023c8ce764a4
jun880381 <144294898+jun880381@users.noreply.github.com>
2023-10-18 12:23:34 +0900

    Add files via upload

--- java/Exam.java
+++ java/Exam.java
@@ -1,5 +1,22 @@
 package org.javaro.lecture;
 
-public class Exam {
-
+public class Exam{
+	String subject;
+	int score;
+	public void setExamScore(String subject, int score) {
+		this.subject = subject;
+		this.score = score;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getSubject() {
+		return this.subject;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
 }
